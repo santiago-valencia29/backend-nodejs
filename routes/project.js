028666afb5c6f41b7f037a4aded4a4f6909f8ee0
edsvalen <edsvalen@bancolombia.com.co>
2020-03-14 15:48:50 -0500

    fix rutas get projects

--- routes/project.js
+++ routes/project.js
@@ -14,7 +14,6 @@ router.get('/test', ProjectController.test);
 router.post('/save-project', ProjectController.saveProject);
 router.get('/project/:id?',ProjectController.getProject);
 router.get('/projects',ProjectController.getProjects);
-router.get('/projects',ProjectController.getProjects);
 router.put('/project/:id',ProjectController.updateProject);
 router.delete('/project/:id',ProjectController.deleteProject);
 router.post('/upload-image/:id',multipartMiddleware,ProjectController.uploadImage);
