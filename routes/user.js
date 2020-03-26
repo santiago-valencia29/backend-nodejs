865561a83a4c1de66ac2bad85e786650c3e65444
edsvalen <edsvalen@bancolombia.com.co>
2020-03-26 01:27:17 -0500

    ok jwt,faltan validaciones adicionales pendiente

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var UserController = require('../controllers/user');
+var jwt = require('jsonwebtoken');
 
 
 var router = express.Router();
@@ -10,13 +11,91 @@ var router = express.Router();
 
 // router.get('/home', ProjectController.home);
 
-router.post('/User', (req, res)=>{
+router.post('/User', (req, res)=>{ // sin usar controller, ejemplo
     res.send('hola Santi');
     console.log(req.body)
 });
 
-router.post('/signup', UserController.signUp);
+router.get('/tasks',(req, res)=>{  // datos publicos
+    res.json([
+        {
+            _id:1,
+            name: 'Task one',
+            description : 'lorep ipsum',
+            date: "2019-11-17t20_39_05"
+
+        },
+        {
+            _id:2,
+            name: 'Task one',
+            description : 'lorep ipsum',
+            date: "2019-11-17t20_39_05"
+
+        },
+        {
+            _id:3,
+            name: 'Task one',
+            description : 'lorep ipsum',
+            date: "2019-11-17t20_39_05"
+
+        }
+    ])
+});
+
+router.get('/private-tasks', verifyToken, (req,res)=>{ // datos privados con veryToken
+    res.json([
+        {
+            _id:1,
+            name: 'Task one',
+            description : 'lorep ipsum',
+            date: "2019-11-17t20_39_05"
+
+        },
+        {
+            _id:2,
+            name: 'Task one',
+            description : 'lorep ipsum',
+            date: "2019-11-17t20_39_05"
+
+        },
+        {
+            _id:3,
+            name: 'Task one',
+            description : 'lorep ipsum',
+            date: "2019-11-17t20_39_05"
+
+        }
+    ])
+});
+
+router.get('/profile', verifyToken,(req, res) => {
+    res.send(req.userId);
+});
+
+//manejo de token:
+router.post('/signup', UserController.signUp); // usando controller
 router.post('/signin', UserController.signIn);
 
 
 module.exports = router;
+
+function verifyToken(req, res, next){
+    // console.log(req.headers.authorization) //en los headers postman autorization y en el value la palabra Bearer y el  token
+    if (!req.headers.authorization){
+        return res.status(401).send('Unthorize Request');
+    };
+
+    var token = req.headers.authorization.split(' ')[1]; //falta si el token es valido
+
+    if (token === 'null'){
+        return res.status(401).send('Unthorize Request');
+    };
+
+    var payload = jwt.verify(token, 'secretkey');
+    // console.log(payload);
+
+    req.userId = payload._id;
+    next(); // simplemente continuar
+
+
+}
